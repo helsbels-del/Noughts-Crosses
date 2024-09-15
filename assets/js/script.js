d6a2d699ecbfab3597306294c1d90f5a74b86aa5
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-09-15 15:11:09 +0000

    Feat: Add const to JS and reset button function

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const boxes = Array.from(documnet.querSelectorAll('.box'));
+const playerCall = documnet.querySelector('.callplayer');
+const replayButton = documnt.querselector('#replay');
+const announcer = document.querySelector('.announcer');
+
+
+
+
+    resetButton.addEventListener('click', resetBoard);
+
