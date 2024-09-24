1a337c01b7fdc0fe5245cbcf89e2cc6f35525de5
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-09-24 18:25:22 +0000

    Fix: Add emptyBoxes array to replayGame function so that the game against pc plays again when you replay. Before it didnt have any empty boxes left at the end of the game so when you repay the computer move didn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,6 +167,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const replayGame = () => {
         game = ['', '', '', '', '', '', '', '', ''];
+        emptyBoxes = [0,1,2,3,4,5,6,7,8];
         isGameActive = true;
         reported.classList.add('hide');
 
